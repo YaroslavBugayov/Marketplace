3df4d02e0c728ef3680f2b7cbf7a462882601ce8
YaroslavBugayov <bugayov576@gmail.com>
2023-11-23 19:22:17 +0200

    final

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let cart = []
+if (localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'))
+    drawCart()
+}
+
 $.ajax({
     url: `https://my-json-server.typicode.com/MrKopchick/ShopRepository/products`,
     dataType: 'json',
@@ -9,9 +15,9 @@ $.ajax({
                     <p>${element.name}</p>
                     <p>Description: ${element.description}</p>
                     <p>Price: ${element.price}<p>
-                    <a href="#">Seller profile</a>]
+                    <a href="profile.html?id=${element.author_id}">Seller profile</a>
                     <br>
-                    <button>Buy</button>
+                    <button onclick="addProduct(${element.id})">Buy</button>
                 </div>
             `);
             console.log(element)
@@ -21,3 +27,45 @@ $.ajax({
         console.error(err)
     }
 })
+
+$('#cart-button').click(() => {
+    $('#cart-products').toggle()
+})
+
+function addProduct(id) {
+    $.ajax({
+        url: `https://my-json-server.typicode.com/MrKopchick/ShopRepository/products/${id}`,
+        dataType: 'json',
+        success: (result) => {
+            cart.push(result)
+            drawCart()
+            localStorage.setItem('cart', JSON.stringify(cart))
+        },
+        error: (error) => {
+            console.error(error)
+        }
+    })
+}
+
+function drawCart() {
+    $('#cart-products').empty()
+    cart.forEach(element => {
+        $('#cart-products').append(`
+            <div class="cart-product">
+                <img src="${element.photo_url}">
+                <p>${element.name}</p>
+                <p>Price: ${element.price}<p>
+            </div>
+        `)
+    })
+    $('#cart-products').append(`
+        <button onclick="buyAll()">Buy all</button>
+    `)
+}
+
+function buyAll() {
+    cart = []
+    $('#cart-products').empty()
+    $('#cart-products').append(`Cart is empty`)
+    localStorage.setItem('cart', '[]')
+}
